a8e8da7afaf8fe1dcaf18182df8262140485c7f2
Matheus Plaza <plazachave.mp7@gmail.com>
2026-01-04 16:19:15 -0300

    feat: handle invalid json format and enum validation errors in global exception handler

--- src/main/java/io/github/matheusplaza/clean_architecture/infra/config/error/GlobalExceptionHandler.java
+++ src/main/java/io/github/matheusplaza/clean_architecture/infra/config/error/GlobalExceptionHandler.java
@@ -4,8 +4,12 @@ import io.github.matheusplaza.clean_architecture.core.exceptions.EventAlreadyExi
 import io.github.matheusplaza.clean_architecture.core.exceptions.EventNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import tools.jackson.databind.exc.InvalidFormatException;
+
+import java.util.Arrays;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -18,6 +22,24 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(EventAlreadyExistsException.class)
     public ResponseEntity<String> handleAlreadyExists(EventAlreadyExistsException e){
         return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleBadRequest(HttpMessageNotReadableException e){
+
+        if (e.getCause() instanceof InvalidFormatException invalidFormat) {
+
+            if (invalidFormat.getTargetType().isEnum()) {
+                String invalidValue = invalidFormat.getValue().toString();
+                String validValues = Arrays.toString(invalidFormat.getTargetType().getEnumConstants());
+
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body(String.format("Invalid value '%s'. The accepted values are: %s",
+                                invalidValue, validValues));
+            }
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Malformed request body");
     }
 
 }
